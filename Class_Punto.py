1d23b4ef2d5edbb044712c1eddbe2cff92ece328
acasasaez <acasasae@myuax.com>
2022-09-21 16:41:13 +0200

    creamos getters y setters

--- Class_Punto.py
+++ Class_Punto.py
@@ -6,3 +6,15 @@ class Punto():
     def init (self, X=0 ,Y=0):
         self.x = X
         self.y = Y
+#Creación de setters and getters: Métodos de acceso a los atributos de una clase
+
+#Creamos los "set" que nos permiten modificar el valor de su respectivo atributo
+    def set_x(self,X=0):
+        self.x=X
+    def set_y(self,Y=0):
+        self.y=Y
+#Creamos los "get" que nos permiten mostrar el valor de su respectivo atributo
+    def get_x(self):
+        return self.x
+    def get_y(self):
+        return self.y
