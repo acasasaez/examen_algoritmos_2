e123cee3a658f6368008094c534c483bd4f19e8c
acasasaez <acasasae@myuax.com>
2022-09-21 16:40:29 +0200

    creamos el constructor

---
+++ Class_Punto.py
@@ -0,0 +1,8 @@
+from cmath import sqrt
+import math
+#Creamos la clase Punto y definimos el método constructor para crear puntos facilmente
+import math
+class Punto():
+    def init (self, X=0 ,Y=0):
+        self.x = X
+        self.y = Y
